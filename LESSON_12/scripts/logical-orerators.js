705b83853115889aaff024ac8ffe1dbaa743b28e
WEXCOM <digital.wexcom@gmail.com>
2019-12-20 22:06:14 +0300

    add: Logical Operators

---
+++ LESSON_12/scripts/logical-orerators.js
@@ -0,0 +1,20 @@
+//console.log(true || true);
+//console.log(true || false);
+//console.log(false || true);
+//console.log(false || false);
+//
+//console.log(true && true);
+//console.log(true && false);
+//console.log(false && true);
+//console.log(false && false);
+
+//console.log(true != false);
+//console.log(!!0);
+
+//console.log(null || 2 || undefined);
+//console.log(alert(1) || 2 || alert(3));
+//console.log(1 && null && 2);
+//console.log(alert('1') && alert('2'));
+//console.log(null || 2 && 3 || 4);
+let age = 70;
+console.log(!(age >= 18 && age < 70));
