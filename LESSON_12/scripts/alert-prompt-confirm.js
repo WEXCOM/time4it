56f626043cf692c5e5beaa52ae73c39f6b81bf3e
WEXCOM <digital.wexcom@gmail.com>
2019-12-20 20:31:40 +0300

    add: Alert Prompt Confirm

---
+++ LESSON_12/scripts/alert-prompt-confirm.js
@@ -0,0 +1,10 @@
+'use strict';
+//let x = alert('Alert Prompt Confirm');
+//let x = prompt('Сколько тебе лет?');
+//let x = confirm('Ты самый умный?');
+//
+//console.log(`Значение Х: ${x}`);
+
+let name = prompt('Как тебя зовут?');
+
+console.log(`Привет, ${name}`);
