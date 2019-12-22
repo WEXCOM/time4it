07bb26fba4c64f094761d8d6c783b49c543a8a11
WEXCOM <digital.wexcom@gmail.com>
2019-12-22 12:35:47 +0300

    add: While and For

---
+++ LESSONS/scripts/while-for.js
@@ -0,0 +1,71 @@
+'use strict';
+
+//let i = 0;
+//
+//do {
+//  console.log(i);
+//  i++;
+//} while (i < 3);
+
+//for (let i=0;i<5;i++) {
+//  console.log(i);
+//}
+
+//let i = 0;
+//
+//for (;;) {
+//  console.log(i);
+//  i++;
+//}
+
+//for (let i=0;i<5;i++) {
+//  if (i == 3) {
+//    console.log('Я нашел тройку');
+//    break;
+//  }
+//  console.log('Это не три...');
+//}
+
+//for (let i=0;i<10;i++) {
+//  if (i%2 == 0) {
+//    console.log(`${i} - четное число`);
+//    continue;
+//  }
+//  console.log(`${i} - не четное число`);
+//}
+
+//iter: for (let i=0;i<=5;i++) {
+//  for (let j=0;j<10;j++) {
+//    let input = confirm(`Ты загодал ${i*10+j}`,'');
+//    if (input) {
+//      alert('Я угодал!');
+//      break iter;
+//    }
+//  }
+//}
+
+//let i = 3;
+//
+//while (i) {
+//  console.log(i);
+//  i--;
+//}
+
+//let i = 0;
+//while (i++ < 5) console.log(i);
+
+//for (let i=0;i<5;i++) console.log(i);
+
+//for (let i=2;i<=10;i++) {
+//  if (i%2==0) console.log(i);
+//}
+
+//for(let i=0;i<3;i++) {
+//  console.log(`Цифра: ${i};`);
+//}
+
+//1. Запрос числа >100
+//2. Запрашивать пока не введет >100 или не нажмет отмену
+//3. Если число <100, то пишет "Ты дурак?"
+//4. Если число >100, пишет "Спасибо!"
+//5. Если нажать "Отмена", то запросы прекратятся.
