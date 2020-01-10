3286b618243c32160180a27799ff17283b610648
WEXCOM <digital.wexcom@gmail.com>
2020-01-10 20:05:30 +0300

    edit: calculator.js add sum and multi functions

---
+++ LESSONS/scripts/calculator/calculator.js
@@ -0,0 +1,11 @@
+'use strict';
+let x = +prompt('Задайте число X:', '0');
+let y = +prompt('Задайте число Y:', '0');
+
+function sum(x,y) {
+  return x + y;
+}
+
+function mult(x,y) {
+  return x * y;
+}
