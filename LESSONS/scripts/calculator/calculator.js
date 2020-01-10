1655cd5a18ea93709c04166b4d1a7f0167a0d82c
WEXCOM <digital.wexcom@gmail.com>
2020-01-10 22:05:36 +0300

    edit: calculator.js learn Objects

--- LESSONS/scripts/calculator/calculator.js
+++ LESSONS/scripts/calculator/calculator.js
@@ -1,11 +1,46 @@
 'use strict';
-let x = +prompt('Задайте число X:', '0');
-let y = +prompt('Задайте число Y:', '0');
+//let x = +prompt('Задайте число X:', '0');
+//let y = +prompt('Задайте число Y:', '0');
+//
+//function sum(x,y) {
+//  return x + y;
+//}
+//
+//function mult(x,y) {
+//  return x * y;
+//}
 
-function sum(x,y) {
-  return x + y;
+// END FIRST VERSION
+
+/* 
+1. Создать пустой объект user
+2. Добавить свойство name со значением "John"
+3. Добавить свойство surname со значением "Smith"
+4. Изменить значение свойства name на Pete
+5. Удалить свойство name из объекта
+*/
+
+/*
+isEmpty(obj) вернет true, если объект пустой и false, если нет.
+
+1. Объявляем функцию;
+2. 
+*/
+
+function isEmpty(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    sum += +obj[key];
+  }
+  return sum;
 }
 
-function mult(x,y) {
-  return x * y;
-}
+/*
+  let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+  
+
+*/
