caacad28a207e26cfd510d3d4a90f1e50cfd2f77
WEXCOM <digital.wexcom@gmail.com>
2020-01-26 10:16:07 +0300

    add: dom.js

---
+++ LESSONS/scripts/dom.js
@@ -0,0 +1,7 @@
+//if (confirm('Перейти на Wikipedia?')) {
+//  location.href='https://wikipedia.org';
+//};
+
+let elem = document.body;
+
+elem.addEventListener('click', ()=>elem.innerHTML = 'Харэ кликать!');
