9c50d95a3da9a8457d9ca365f6a1441e3914c7d8
WEXCOM <digital.wexcom@gmail.com>
2020-01-26 13:12:36 +0300

    add: dom.js

--- LESSONS/scripts/dom.js
+++ LESSONS/scripts/dom.js
@@ -2,6 +2,58 @@
 //  location.href='https://wikipedia.org';
 //};
 
-let elem = document.body;
+//let list = {
+//  doctype: "<!DOCTYPE html>",
+//  html: {
+//    type: "element",
+//    text: {
+//      type: "text",
+//      value: " ",
+//    },
+//    head: {
+//      type: "element",
+//      meta: {
+//        type: "element",
+//        charset: "UTF-8",
+//      },
+//      title: {
+//        text: {
+//          type: text,
+//          value: "LESSON_12",
+//        }
+//      },
+//    },
+//    body: {
+//      type: "element",
+//      ul: {
+//        type: "element",
+//        li: {
+//          type: "element",
+//          value: [
+//                    "Apple", "Banana", "Orange", "Limon", "Mango"
+//                  ],
+//      },
+//      script: {
+//        type: "element",
+//        src: "scripts/dom.js",
+//      },
+//    },
+//  },
+//}
 
-elem.addEventListener('click', ()=>elem.innerHTML = 'Харэ кликать!');
+//for (let i = 0; i < document.body.childNodes.length; i++) {
+//  console.log(document.body.childNodes[i]);
+//}
+
+//for (let node of document.body.childNodes) {
+//  console.log(node);
+//}
+
+// Задание №1
+
+let table = document.getElementById('supertable');
+
+for (let i = 0; i < table.rows.length; i++) {
+  let row = table.rows[i];
+  row.cells[i].style.backgroundColor = 'red';
+}
